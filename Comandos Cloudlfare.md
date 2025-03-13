829d1e33d7583a1664f41c20d6ea66ae92944484
Foxy20122012 <malvarezm26@miumg.edu.gt>
2025-03-13 13:03:10 -0600

    first commit

---
+++ Comandos Cloudlfare.md
@@ -0,0 +1,11 @@
+## Iniciar session
+npx wrangler login
+
+## Compilar
+npm run build
+
+## Publicar
+npx wrangler publish
+
+
+
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@tailwindcss/vite";
+import react from "@astrojs/react";
+import cloudflare from "@astrojs/cloudflare"; 
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: cloudflare(), 
+  vite: {
+    plugins: [tailwind()],
+  },
+  integrations: [react()],
+});
