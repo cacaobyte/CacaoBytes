5eee8dbd064a9fca8d3a3cfe02f497c8d53a6671
Foxy20122012 <malvarezm26@miumg.edu.gt>
2025-03-13 21:18:23 -0600

    Ruta home

---
+++ src/Models/Welcome/welcomeModels.jsx
@@ -0,0 +1,16 @@
+
+export const services = [
+    { icon: "🌐", color: "text-blue-600 border-blue-500", title: "Desarrollo Web", description: "Diseñamos experiencias digitales que impulsan negocios." },
+    { icon: "⚙️", color: "text-purple-600 border-purple-500", title: "Software a la Medida", description: "Soluciones tecnológicas adaptadas a cada necesidad." },
+    { icon: "🔒", color: "text-yellow-600 border-yellow-500", title: "Seguridad Informática", description: "Protege tus sistemas con auditorías y capacitación." },
+    { icon: "☁️", color: "text-green-600 border-green-500", title: "Software como Servicio (SaaS)", description: "Plataformas escalables y accesibles en la nube." },
+    { icon: "🚀", color: "text-red-600 border-red-500", title: "Transformación Digital", description: "Impulsamos la modernización de empresas con tecnología." },
+    { icon: "📊", color: "text-blue-600 border-blue-500", title: "Inteligencia de Negocios", description: "Analizamos datos y potenciamos la toma de decisiones." }
+  ];
+  
+  export const advantages = [
+    { icon: "💡", color: "text-yellow-500 border-yellow-400", title: "Innovación Constante", description: "Desarrollamos soluciones tecnológicas avanzadas." },
+    { icon: "⚡", color: "text-orange-500 border-orange-400", title: "Eficiencia y Calidad", description: "Optimizamos recursos y maximizamos resultados." },
+    { icon: "🤝", color: "text-blue-500 border-blue-400", title: "Compromiso Total", description: "Tu éxito es nuestra prioridad, trabajamos contigo." }
+  ];
+  
